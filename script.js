83da21b03ae026b9cc718821fc0d9eecae9c5b19
Omar Alminagorta <alminagorta@users.noreply.github.com>
2026-03-02 22:21:33 -0700

    ver2

--- script.js
+++ script.js
@@ -1,45 +1,124 @@
-const svg = d3.select("svg");
-const width = 600;
-const height = 400;
-const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+const margin = { top: 20, right: 40, bottom: 40, left: 60 };
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
-const chartWidth = width - margin.left - margin.right;
-const chartHeight = height - margin.top - margin.bottom;
-
-const g = svg.append("g")
+const svg = d3.select("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
-d3.csv("data.csv").then(data => {
+const tooltip = d3.select(".tooltip");
 
+// Parse the date
+const parseTime = d3.timeParse("%Y-%m-%d");
+
+d3.csv("data.csv").then(function(data) {
+  // Format the data
   data.forEach(d => {
+    d.date = parseTime(d.date);
     d.value = +d.value;
   });
 
-  const x = d3.scaleBand()
-    .domain(data.map(d => d.name))
-    .range([0, chartWidth])
-    .padding(0.1);
+  // Set the scales
+  const x = d3.scaleTime()
+    .domain(d3.extent(data, d => d.date))
+    .range([0, width]);
 
   const y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.value)])
-    .nice()
-    .range([chartHeight, 0]);
+    .range([height, 0]);
 
-  g.append("g")
-    .attr("transform", `translate(0,${chartHeight})`)
-    .call(d3.axisBottom(x));
+  // Add X axis
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x).ticks(d3.timeDay.every(1)))
+    .selectAll(".tick text")
+    .attr("class", "axis-label");
 
-  g.append("g")
-    .call(d3.axisLeft(y));
+  // Add Y axis
+  svg.append("g")
+    .call(d3.axisLeft(y))
+    .selectAll(".tick text")
+    .attr("class", "axis-label");
 
-  g.selectAll(".bar")
+  // Create the line function
+  const line = d3.line()
+    .x(d => x(d.date))
+    .y(d => y(d.value));
+
+  // Add the line path
+  svg.append("path")
+    .data([data])
+    .attr("class", "line")
+    .attr("d", line);
+
+  // Add circles to the line (interactive)
+  const circles = svg.selectAll("circle")
     .data(data)
     .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", d => x(d.name))
-    .attr("y", d => y(d.value))
-    .attr("width", x.bandwidth())
-    .attr("height", d => chartHeight - y(d.value));
+    .append("circle")
+    .attr("cx", d => x(d.date))
+    .attr("cy", d => y(d.value))
+    .attr("r", 5)
+    .attr("fill", "steelblue")
+    .on("mouseover", function(event, d) {
+      tooltip.style("visibility", "visible")
+        .text(`Date: ${d3.timeFormat("%Y-%m-%d")(d.date)} | Value: ${d.value}`);
+    })
+    .on("mousemove", function(event) {
+      tooltip.style("top", (event.pageY - 10) + "px")
+        .style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", function() {
+      tooltip.style("visibility", "hidden");
+    });
 
+  // Dynamic update function (for example, based on button click or new data arrival)
+  function updateChart(newData) {
+    // Re-parse and reformat the new data
+    newData.forEach(d => {
+      d.date = parseTime(d.date);
+      d.value = +d.value;
+    });
+
+    // Update scales
+    x.domain(d3.extent(newData, d => d.date));
+    y.domain([0, d3.max(newData, d => d.value)]);
+
+    // Update the line
+    svg.select(".line")
+      .data([newData])
+      .transition()
+      .duration(750)
+      .attr("d", line);
+
+    // Update the circles
+    const circleUpdate = svg.selectAll("circle")
+      .data(newData);
+
+    circleUpdate.enter()
+      .append("circle")
+      .attr("cx", d => x(d.date))
+      .attr("cy", d => y(d.value))
+      .attr("r", 5)
+      .attr("fill", "steelblue")
+      .merge(circleUpdate)
+      .transition()
+      .duration(750)
+      .attr("cx", d => x(d.date))
+      .attr("cy", d => y(d.value));
+
+    circleUpdate.exit().remove();
+  }
+
+  // Example of dynamically updating the chart with new data (can be triggered by a button or external event)
+  setTimeout(() => {
+    const newData = [
+      { date: "2023-01-08", value: 110 },
+      { date: "2023-01-09", value: 95 },
+      { date: "2023-01-10", value: 80 }
+    ];
+    updateChart(newData);
+  }, 5000);
 });
